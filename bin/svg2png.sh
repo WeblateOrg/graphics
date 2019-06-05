55bfe60d65603ce73f6edf1d5d0c5bb8b3a44455
Michal Čihař <michal@cihar.com>
2019-06-05 15:05:44 +0200

    Support changing background
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- bin/svg2png.sh
+++ bin/svg2png.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ $# -ne 6 ] ; then
-    echo "Usage: svg2png.sh image.svg image.png width height background-opacity colors"
+if [ $# -ne 7 ] ; then
+    echo "Usage: svg2png.sh image.svg image.png width height background-opacity background-color colors"
     exit 1
 fi
 
-inkscape $1 --export-width=$3 --export-background-opacity=$5 --export-height=$4 --export-png=tmp.png
-pngnq -s 1 -f -n $6 tmp.png
+inkscape $1 --export-width=$3 --export-background-opacity=$5 --export-background=$6 --export-height=$4 --export-png=tmp.png
+pngnq -s 1 -f -n $7 tmp.png
 zopflipng -m tmp-nq8.png $2
 rm tmp.png tmp-nq8.png
