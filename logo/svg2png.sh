50767b055e9b9241aaa9f7fd3f92a74f50a75f44
Michal Čihař <michal@cihar.com>
2019-06-05 14:56:21 +0200

    Use one base image for all
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- logo/svg2png.sh
+++ logo/svg2png.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ $# -ne 5 ] ; then
-    echo "Usage: svg2png.sh image.svg image.png width height colors"
+if [ $# -ne 6 ] ; then
+    echo "Usage: svg2png.sh image.svg image.png width height background-opacity colors"
     exit 1
 fi
 
-inkscape $1 --export-width=$3  --export-height=$4 --export-png=tmp.png
-pngnq -s 1 -f -n $5 tmp.png
+inkscape $1 --export-width=$3 --export-background-opacity=$5 --export-height=$4 --export-png=tmp.png
+pngnq -s 1 -f -n $6 tmp.png
 zopflipng -m tmp-nq8.png $2
 rm tmp.png tmp-nq8.png
